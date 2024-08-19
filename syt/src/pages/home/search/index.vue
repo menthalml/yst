9fa51620863fef8b09c44bbe586f869314dc2880
梁梦林 <liangmenglin@tuhuadmindeMacBook-Pro.local>
2024-08-19 19:52:53 +0800

    feat: 搜索

--- syt/src/pages/home/search/index.vue
+++ syt/src/pages/home/search/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search">
         <!-- :fetch-suggestions="querySearch" -->
-      <el-autocomplete class="form" clearable placeholder="请输入医院名称"/>
+      <el-autocomplete clearable placeholder="请输入医院名称"/>
       <el-button type="primary" size="default" :icon="Search">搜索</el-button>
   </div>
 </template>
@@ -11,6 +11,7 @@ import { Search } from '@element-plus/icons-vue';
 
 <style scoped lang="scss">
   // 深度选择器 css: >>>, less: /deep/, scss: v-deep
+
 .search {
   width: 100%;
   height: 50px;
@@ -18,8 +19,12 @@ import { Search } from '@element-plus/icons-vue';
   justify-content: center;
   align-items: center;
   margin: 10px 0;
-  ::v-deep .el-input__wrapper {
-    width: 300px;
+  ::v-deep(.el-input__wrapper) {
+    width: 600px !important;
+  }
+  ::v-deep(.el-autocomplete) {
+    width: 600px !important;
+    margin-right: 10px;
   }
 }
 
